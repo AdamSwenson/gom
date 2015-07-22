3b310a2733b650e4a6170f6577b44f8a39844e53
yreej <jerrysmash17@gmail.com>
2015-07-22 12:04:55 -0700

    Login system partially works. It uses forms to trnasfer data via the landing controller. The database is not used.

--- resources/views/setup/edit_question.blade.php
+++ resources/views/setup/edit_question.blade.php
@@ -24,21 +24,39 @@
 <div id="editQuestion">
     <div class="section">
         <div class="container">
+
+
             <nav>
                 <ul class="pager">
                     <li class="next">
-                        <a href="#">Done <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
+                            <form method="GET" action="{{url('/element')}}" accept-charset="UTF-8" class="col-xs-8">
+                            <div class="col-sm-11">
+                            </div>
+
+                            <div class="col-sm-1">
+                                    <!-- <input type="submit" name="examid" value="$examid}}"> -->
+                                <button type="submit" name="examid" value="{{$examid}}" class="btn btn-default btn-lg"> Next
+                                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                </button>
+                            </div>
+                            </form>
                     </li>
                 </ul>
             </nav>
-            <h2 id="examName">Exam Name: Add / Edit Questions</h2>
+            <div>
+            <h2 id="examName"> {{ $examid}}  : Add / Edit Questions</h2>
             <h5>Add the questions that will appear on this exam. When you're finished, press "done".</h5>
-
+            </div>
             <!-- this Div will become the question template -->
-            @include('setup.question_form')
+            <?php $num = 1; ?>
+            @foreach($numOfQuestions as $question)
+                @include('setup.question_form')
+                <?php $num += 1; ?>
+            @endforeach
             <br>
-            <button class="btn btn-primary" id="addQuestion"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-             Add Question</button>
+            <a class="btn btn-primary" id="addQuestion"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+             Add Question</a>
+
             <button class="btn btn-primary" id="importQuestion"><span class="glyphicon glyphicon-import" aria-hidden="true"></span>
              Import Question</button>
         </div>
