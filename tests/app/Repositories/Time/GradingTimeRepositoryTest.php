b6a591ead7a3db3d91a2220dc9025143a58ba1b3
AdamSwenson <adam.swenson@csun.edu>
2015-08-21 14:54:28 -0700

    added method for recording total grading time without adding to preexisting time

---
+++ tests/app/Repositories/Time/GradingTimeRepositoryTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 8/21/15
+ * Time: 2:40 PM
+ */
+
+namespace App\Repositories\Time;
+
+
+class GradingTimeRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $object;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->object = new GradingTimeRepositoryTest;
+    }
+
+}
