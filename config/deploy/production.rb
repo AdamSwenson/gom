ab2ac2df3d766876bce528e8d8607b526135ad7d
AdamSwenson <adam.swenson@csun.edu>
2015-09-11 16:04:09 -0700

    Added capistrano files for deployment to server

---
+++ config/deploy/production.rb
@@ -0,0 +1,63 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+server 'gradeomatic.net', user: 'nico', roles: %w{app db web}, my_property: :my_value
+#server 'gradeomatic.net', user: 'nico', roles: %w{web}, my_property: :my_value
+
+# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+# server 'db.example.com', user: 'deploy', roles: %w{db}
+
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any  hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+role :web, %w{nico@gradeomatic.net gradeomatic.net}
+#role :web, %w{nico@45.75.99.151 gradeomatic.net}
+
+# role :app, %w{deploy@example.com}, my_property: :my_value
+#role :web, %w{nico@45.75.99.151}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
+
+
+
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
+
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+  set :ssh_options, {
+    keys: %w(/Users/adam/.ssh/id_rsa.pub),
+    forward_agent: true,
+    auth_methods: %w(publickey 2smellpls2)
+  }
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
