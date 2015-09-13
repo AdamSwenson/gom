d4d1cbf985bb8347b0fa401d90881c14876be684
AdamSwenson <adam.swenson@csun.edu>
2015-09-12 22:00:22 -0700

    working on mail

--- app/Jobs/Feedback/NotifySingleStudent.php
+++ app/Jobs/Feedback/NotifySingleStudent.php
@@ -55,7 +55,7 @@ class NotifySingleStudents extends Job implements SelfHandling, ShouldQueue
 
     public function handle()
     {
-        $this->helper->sendEmailToStudent(Exam::findOrFail($this->exam), Student::findOrFail($this->student));
+        $this->helper->sendEmailToStudent(Exam::findOrFail($this->exam->id), Student::findOrFail($this->student->id));
 
     }
 }
