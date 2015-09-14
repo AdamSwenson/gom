8d607302c0d948f6bef6e2bdfe292dd0cf385263
AdamSwenson <adam.swenson@csun.edu>
2015-09-13 17:55:17 -0700

    Deleted service provider seems to be causing problems. Restoring service provider to try to fix

---
+++ app/Providers/UtilitiesServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 9/13/15
+ * Time: 5:54 PM
+ */
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class UtilitiesServiceProvider
+ *
+ * This handles management and registration of various tools
+ * like cleaners.
+ *
+ * @package App\Providers
+ */
+class UtilitiesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
