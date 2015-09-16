9efd0c473950cb99a316a76ea06fab41f8b15f62
AdamSwenson <adam.swenson@csun.edu>
2015-09-16 16:58:52 -0700

    Changed cdns to https. Downloaded library files in case want to compile and minify all of them. However, for now, still pulling libraries from cdns

--- resources/views/layouts/js_bootstrap_loader.blade.php
+++ resources/views/layouts/js_bootstrap_loader.blade.php
@@ -1,2 +1,4 @@
-<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+{{--<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>--}}
+{{--<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">--}}
