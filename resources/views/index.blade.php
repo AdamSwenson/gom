bba94788c79e7445bde987050f6f1516a78bd90b
AdamSwenson <adam.swenson@csun.edu>
2015-09-16 17:21:49 -0700

    of course. we can't just simply switch to https can we? no. The morass that is the landing page (which loads 9000 versions of the same library from different places) just won't stand for that. Trying to roll back slightly and unfuck it.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,14 +19,13 @@
     <div id="scriptBox">
         @include('layouts.js_jquery_loader')
         @include('layouts.js_scriptloader')
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-        <script type="text/javascript" src="<?php echo asset("inc/js/bootbox.min.js");?>"></script>
         <!-- bootbox for easy modals -->
         <script type="text/javascript" src="<?php echo asset("inc/js/bootbox.min.js");?>"></script>
         <!--  rubaXA Sortable for drag and drop -->
-        <script src="http://rubaxa.github.io/Sortable/Sortable.js"></script>
+        <script src="{{ asset('inc/js/Sortable.js') }}"></script>
 
         <script type="text/javascript">
             $.ajaxSetup({
--- resources/views/layouts/js_additional_libs.blade.php
+++ resources/views/layouts/js_additional_libs.blade.php
@@ -1,6 +1,6 @@
 <!-- extra libs: boostrap-slider, bootbox, sortable -->
 <link href="{{ asset('inc/css/bootstrap-slider.css') }}" rel="stylesheet">
-<script type="text/javascript" src="<?php echo asset("inc/js/bootbox.min.js");?>"></script>
+<script type="text/javascript" src="{{ asset("inc/js/bootbox.min.js") }}"></script>
 <script src="{{ asset("inc/js/Sortable.js") }}"></script>
 
 {{--http://rubaxa.github.io/Sortable/Sortable.js--}}
