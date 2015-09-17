823b1a098762f50059da2d40ebc643b0153f1412
AdamSwenson <adam.swenson@csun.edu>
2015-09-16 17:46:53 -0700

    oy vey

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,8 +17,8 @@
     </script>
 
     <div id="scriptBox">
-        @include('layouts.js_jquery_loader')
-        @include('layouts.js_scriptloader')
+        {{--@include('layouts.js_jquery_loader')--}}
+        {{--@include('layouts.js_scriptloader')--}}
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
