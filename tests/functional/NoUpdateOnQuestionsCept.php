82e581500073090ab06e2f044b6bfc72bd9e4bff
AdamSwenson <adam.swenson@csun.edu>
2015-09-27 13:38:31 -0700

    working functional test for question edit page

--- tests/functional/NoUpdateOnQuestionsCept.php
+++ tests/functional/NoUpdateOnQuestionsCept.php
@@ -1,31 +1,39 @@
 <?php 
 $I = new FunctionalTester($scenario);
+
 $I->wantTo('click past the question edit page and have nothing change');
 
 \Illuminate\Support\Facades\Auth::loginUsingId(1);
+
 $I->disableMiddleware();
 
-$I->amOnPage('/exam');
-
-//$I->seeInTitle("Exam Setup | gradeomatic");
-
-
+/* --------------------------------------- Open the page */
 $I->amOnPage('/exam/1/question/edit');
 
-
+/* --------------------------------------- Check that opened correctly */
 $I->seeInTitle("Edit Questions | gradeomatic");
-
+//See page description
 $I->see("Add / Edit Questions");
+//Make sure see nav button
 $I->see("Add / Edit Elements");
 
-$I->click("Add / Edit Elements");
+/* --------------------------------------- Submit the page */
+$I->submitForm('#questionForm', []);
 
+//Check that sent to next page
 $I->canSeeResponseCodeIs(200);
-//$I->click('#next');
-//$I->click('Add / Edit Elements <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>');
-//$I->click(['css' => 'a.glyphicon.glyphicon-chevron-right']);
-//$I->submitForm('#questionForm', []);
-//$I->executeJS("jQuery('#questionForm').submit()");
-//$I->executeJS("submitForm('editElements');");
-//$I->seeInDatabase('question_assignments', ['exam_id' => 1, 'question_id' => 1, 'question_number' => 1]);
-//$I->seeInDatabase('question_assignments', ['exam_id' => 1, 'question_id' => 2, 'question_number' => 2]);
+$I->seeInTitle("Edit Elements | gradeomatic");
+
+//Verify that assignments not altered
+$I->seeInDatabase('question_assignments', ['exam_id' => 1, 'question_id' => 1, 'question_number' => 1]);
+$I->seeInDatabase('question_assignments', ['exam_id' => 1, 'question_id' => 2, 'question_number' => 2]);
+$I->seeInDatabase('question_assignments', ['exam_id' => 1, 'question_id' => 3, 'question_number' => 3]);
+$I->seeInDatabase('question_assignments', ['exam_id' => 1, 'question_id' => 4, 'question_number' => 4]);
+$I->seeInDatabase('question_assignments', ['exam_id' => 1, 'question_id' => 5, 'question_number' => 5]);
+
+//Verify that question scores not deleted
+$I->seeInDatabase('question_scores', ['question_assignment_id' => 1]);
+$I->seeInDatabase('question_scores', ['question_assignment_id' => 2]);
+$I->seeInDatabase('question_scores', ['question_assignment_id' => 3]);
+$I->seeInDatabase('question_scores', ['question_assignment_id' => 4]);
+$I->seeInDatabase('question_scores', ['question_assignment_id' => 5]);
