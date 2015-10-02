1998ea72544834d163a6b18ddf3ee9aecc365266
AdamSwenson <adam.swenson@csun.edu>
2015-10-02 13:12:42 -0700

    Fixed bad link in welcome email

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -14,12 +14,12 @@
 
 <p>Soon you'll be grading faster, giving your students helpful comments, and collecting the data that will help you teach better.</p>
 
-<p><a href="{{ url('/gettingStarted') }}">Getting started guide</a></p>
+<p><a href="https://www.gradeomatic.net/gettingStarted">Getting started guide</a></p>
 
 <p>Thank you,<br />
     The Gradeomatic Team
 </p>
 <p>
-    <a href="{{ url('/') }}">{{ url('/') }}</a>
+    <a href="https://www.gradeomatic.net">https://www.gradeomatic.net</a>
 </p>
 </html>
