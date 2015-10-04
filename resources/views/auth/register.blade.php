f4e38653401bb10dfdceabfaf1b5959e672d9986
brian_barsda <bbarsda@gmail.com>
2015-10-03 17:13:20 -0700

    Changed account.home.blade to account.settings. Refined that page's display. Centered login and create divs for home page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,27 +6,29 @@
 
 @section('body')
     <div class="row">
-    <form role="form" method="POST" action="{{url('auth/register')}}" accept-charset="UTF-8" class="col-xs-4">
-        {!! csrf_field() !!}
-        <h3 class="text-left"> Create Account</h3>
+        <div class="col-xs-3"></div>
+        <form role="form" method="POST" action="{{url('auth/register')}}" accept-charset="UTF-8" class="col-xs-6">
+            {!! csrf_field() !!}
+            <h3 class="text-left"> Create Account</h3>
 
-        <div class="form-group ">
-            <input class="form-control" type="text" name="name" value="{{ old('name') }}"
-                   placeholder="Username">
-        </div>
-        <div class="form-group ">
-            <input class="form-control" type="email" name="email" value="{{ old('email') }}"
-                   placeholder="Enter email">
-        </div>
-        <div class="form-group">
-            <input class="form-control" type="password" name="password" id="pwd" placeholder="Enter password">
-        </div>
-        <div class="form-group">
-            <input class="form-control" type="password" name="password_confirmation" id="pwd_conf"
-                   placeholder="Confirm password">
-        </div>
+            <div class="form-group ">
+                <input class="form-control" type="text" name="name" value="{{ old('name') }}"
+                       placeholder="Username">
+            </div>
+            <div class="form-group ">
+                <input class="form-control" type="email" name="email" value="{{ old('email') }}"
+                       placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" name="password" id="pwd" placeholder="Enter password">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" name="password_confirmation" id="pwd_conf"
+                       placeholder="Confirm password">
+            </div>
 
-        <input class="btn btn-default" value="Create Account" type="submit">
-    </form>
+            <input class="btn btn-primary" value="Create Account" type="submit">
+        </form>
+        <div class="col-xs-3"></div>
     </div>
 @endsection
