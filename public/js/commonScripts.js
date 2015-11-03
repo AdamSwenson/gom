5a9db0f6bc7671f2c262cc0b5735a8308d1ae6be
AdamSwenson <adam.swenson@csun.edu>
2015-11-03 09:37:15 -0800

    added letter grade button (with minified scripts in production)

--- public/js/commonScripts.js
+++ public/js/commonScripts.js
@@ -1,7 +1,1 @@
-/**
- * Created by adam on 10/4/15.
- */
-
-//Automatically hide non-important flash message
-$('div.alert').not('alert-important').delay(3000).slideUp(300);
-//# sourceMappingURL=commonScripts.js.map
+$("div.alert").not("alert-important").delay(3e3).slideUp(300);
