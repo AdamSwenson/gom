c3f4ed59af410251eb02d67b50dac722c2e01191
AdamSwenson <adam.swenson@csun.edu>
2016-03-03 09:50:32 -0800

    fixed problem with analytics js and other stuff

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -22,7 +22,7 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param  \Exception $e
      * @return void
      */
     public function report(Exception $e)
@@ -33,12 +33,20 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $e
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $e)
     {
+        //Respond via ajax if was an ajax request
+        if ( $request->ajax() )
+        {
+//            ['status' => 'fail', 'message' => 'There was a problem'];
+//            redic
+//            return
+        }
+
         return parent::render($request, $e);
     }
 }
--- resources/views/grade/partials/element_panel.blade.php
+++ resources/views/grade/partials/element_panel.blade.php
@@ -3,18 +3,27 @@
      style="background-color: #DDDDDD;"
      id="element{{ $elementIndex }}"
      data-element-index="{{ $elementIndex }}"
-     data-element-id="{{ $elements[$eNumber-1]->getId() }}">
+     data-element-id="{{ $elements[$eNumber-1]->getId() }}"
+>
     <h5>Element #{{ $eNumber }}: "{{ $elements[$eNumber-1]->getElementName() }}"</h5>
     <div class="row">
                 <span class="col-lg-5" style="padding-right: 0px;">
                     <!-- score slider -->
                     <label for="sliderQ{{ $qNumber }}E{{ $eNumber }}"></label>
-                    <input class="slider" id="sliderQ{{ $qNumber }}E{{ $eNumber }}" type="text"/>
+                    <input
+                            class="slider"
+                            id="sliderQ{{ $qNumber }}E{{ $eNumber }}"
+                            type="text"
+                    />
                 </span>
-                    <!-- comment area -->
-                <span class="col-lg-7" style="background-color: #DDDDDD; padding-left: 0;">
-                    <textarea class="form-control" rows="4" name="commentQ{{ $qNumber }}E{{ $eNumber }}" placeholder=
-                    "No score for this element"></textarea>
+        <!-- comment area -->
+                <span class="col-lg-7" style="background-color: #DDDDDD; padding-left: 0px; padding-right:0px;">
+                    <textarea
+                            class="form-control"
+                            rows="4"
+                            name="commentQ{{ $qNumber }}E{{ $eNumber }}"
+                            placeholder="No score for this element"
+                    ></textarea>
                 </span>
     </div>
 </div>
