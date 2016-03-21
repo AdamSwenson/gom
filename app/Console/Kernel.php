79dd0e2f210bc4926d4519206a9cd61a29422754
AdamSwenson <adam.swenson@csun.edu>
2016-03-21 16:28:20 -0700

    backup to run every 5 min

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Repositories\Utilities\IBackupFlagRepository;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
