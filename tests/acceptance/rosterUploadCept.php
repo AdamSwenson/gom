a795202dfb47490a5dc1bdcf823d819d82edbaca
AdamSwenson <adam.swenson@csun.edu>
2016-04-11 12:29:33 -0700

    more minor work. still having problemswith deletion after one row deleted

--- tests/acceptance/rosterUploadCept.php
+++ tests/acceptance/rosterUploadCept.php
@@ -5,6 +5,16 @@ $examId = 1;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('upload a csv file full of students and see the students in the database');
 
-
+//Log in
 $I->test_login($I);
+
+//Go to page
+$I->amOnPage('/exam/1/student/edit');
+
+$I->wait(10);
+
+//Make sure seeing what should
+$I->seeInTitle('Edit Roster | gradeomatic');
+
+$I->attachFile('#fileInput', 'acceptance_test_roster.csv');
 
