188e66e5dec1f56e7f611adc56c101d0f443760a
AdamSwenson <adam.swenson@csun.edu>
2016-06-02 09:26:32 -0700

    Pretty much all grading js and tests working okay (except for stupid slider based errors). Getting ready to merge into dev.

--- public/js/grade-exam-package.js
+++ public/js/grade-exam-package.js
@@ -28,66 +28,68 @@ window.onload = function () {
 
     // /* -------------------------------- GENERAL FUNCTIONS ------------------------------ */
 
-    /**
-     * Called when an element slider stops movement. Updates element
-     * score and text (if necessary), then saves score, text and time
-     * @param slideEvt
-     */
-    function handleElementSliderStopEvent(slideEvt, data, SliderTools, Roster, AjaxHandler, Dashboard) {
-        //grab the info related to elements
-        var $element = $(slideEvt.target).closest('[id^="element"]');
-        var $parent = $element.parents('[id^="element"]');
-        var commentAreaId = $element.attr('data-comment-area-id');
-        var $elementComment = $('#' + commentAreaId);
-        var elementIndex = $element.attr('data-element-index'); //the subtask number of the element
-        var elementId = $element.attr('data-element-id'); //the DB's id for the element
-
-        //grab scores
-        var oldScore = data.getElementScore(Roster.activeStudent, elementIndex);
-        var score = slideEvt.value;
-
-        /* ---------- update the element's score visually and in data.elementScores[] --------- */
-
-        //store the new element score in the data object
-        data.storeElementScore(Roster.activeStudent, elementIndex, score);
-
-        /**
-         * update comment text and save to DB.
-         * Only replace text if the score has changed valence regions
-         */
-        if (!SliderTools.isSameValence(oldScore, score)) {
-            //Score is in a new valence region.
-            //So let's plug in the appropriate comment text and save to DB
-
-            //Store comment text in data object
-            //Dear Adam, make sure you read the doc for storeCommentText before fucking with
-            //anything in these lines
-            data.storeCommentText(Roster.activeStudent, elementIndex, $elementComment.val());
-            var commentText = data.getCommentText(Roster.activeStudent, elementIndex, SliderTools.getValence(score));
-
-            //update display
-            updateDisplayedComment($elementComment, commentText);
-
-            //send to the db
-            AjaxHandler.saveComment(data, Roster, elementId, score, commentText);
-        } else {
-            // Score is in the same valence region.
-            // Jump straight to saving without changing the elementComment
-            // Fear not. Changes directly to the comment text will be handled elsewhere.
-            AjaxHandler.createGradeRequest(data, 'element_id', elementId, score, null, Roster);
-        }
-
-        // If using bell curve (standardScoring), element score affects
-        // the total question score, so update
-        if (Roster.standardScoring) {
-            updateStandardScores();
-        }
-
-        //Update dashboard and roster data displayed
-        updateStudentDashboardAndRosterAreas(data, Dashboard, Roster);
-        //Sigh. The user forgot to restart the timer. Do it for them
-        Timer.resumeTimerIfPaused(data, Roster, Dashboard);
-    }
+    // /**
+    //  * Called when an element slider stops movement. Updates element
+    //  * score and text (if necessary), then saves score, text and time
+    //  * @param slideEvt
+    //  */
+    // function handleElementSliderStopEvent( slideEvt, data, SliderTools, Roster, AjaxHandler, Dashboard ) {
+    //     //grab the info related to elements
+    //     var $element = $( slideEvt.target ).closest( '[id^="element"]' );
+    //     var $parent = $element.parents( '[id^="element"]' );
+    //     var commentAreaId = $element.attr( 'data-comment-area-id' );
+    //     var $elementComment = $( '#' + commentAreaId );
+    //     var elementIndex = $element.attr( 'data-element-index' ); //the subtask number of the element
+    //     var elementId = $element.attr( 'data-element-id' ); //the DB's id for the element
+    //
+    //     //grab scores
+    //     var oldScore = data.getElementScore( Roster.activeStudent, elementIndex );
+    //     var score = slideEvt.value;
+    //
+    //     /* ---------- update the element's score visually and in data.elementScores[] --------- */
+    //
+    //     //store the new element score in the data object
+    //     data.storeElementScore( Roster.activeStudent, elementIndex, score );
+    //
+    //
+    //     /**
+    //      * update comment text and save to DB.
+    //      * Only replace text if the score has changed valence regions
+    //      */
+    //     if ( ! SliderTools.isSameValence( oldScore, score ) ) {
+    //         //Score is in a new valence region.
+    //         //So let's plug in the appropriate comment text and save to DB
+    //
+    //         //Store comment text in data object
+    //         //Dear Adam, make sure you read the doc for storeCommentText before fucking with
+    //         //anything in these lines
+    //         data.storeCommentText( Roster.activeStudent, elementIndex, $elementComment.val() );
+    //         var commentText = data.getCommentText( Roster.activeStudent, elementIndex, SliderTools.getValence( score ) );
+    //
+    //         //update display
+    //         updateDisplayedComment( $elementComment, commentText );
+    //
+    //         //send to the db
+    //         AjaxHandler.saveComment( data, Roster, elementId, score, commentText );
+    //
+    //     } else {
+    //         // Score is in the same valence region.
+    //         // Jump straight to saving without changing the elementComment
+    //         // Fear not. Changes directly to the comment text will be handled elsewhere.
+    //         AjaxHandler.createGradeRequest( data, 'element_id', elementId, score, null, Roster );
+    //     }
+    //
+    //     // If using bell curve (standardScoring), element score affects
+    //     // the total question score, so update
+    //     if ( Roster.standardScoring ) {
+    //         updateStandardScores();
+    //     }
+    //
+    //     //Update dashboard and roster data displayed
+    //     updateStudentDashboardAndRosterAreas( data, Dashboard, Roster );
+    //     //Sigh. The user forgot to restart the timer. Do it for them
+    //     Timer.resumeTimerIfPaused( data, Roster, Dashboard );
+    // }
 
     /**
      * Changes the text of the displayed comment
@@ -223,16 +225,6 @@ window.onload = function () {
             }
         });
     }
-
-    /*
-     * The problem:
-     * Add custom text to an element.
-     * Switch to a different user.
-     * Switch back to the first user
-     * Go to the question where added text to element
-     * ----> shows stock text
-     * If reload page, will show custom text
-     * */
 
     /**
      * sums elements scores and sets question scores - will be
