0cb7e03e40b012ec66b14bbe69642d258354be99
AdamSwenson <adam.swenson@csun.edu>
2016-06-11 18:54:07 -0400

    Disabled the setQuestionTask method on Element. It seems not to be used because the more sophisticated elementassignmentrepository process takes care of it.

--- app/Element.php
+++ app/Element.php
@@ -32,42 +32,42 @@ class Element extends BaseModel
 
 # -------------- getters and setters
 
-    /**
-     * Records the element as a subtask of an assigned question.
-     *
-     *
-     * @param $examId
-     * @param $questionId
-     * @param $subtask
-     * @return $this
-     */
-    public function setAsQuestionTask($examId, $questionId, $subtask)
-    {
-        /*
-        Check to see if the element is already assigned on the exam.
-        If so, remove it so that there will only be one of an element per exam.
-        */
-//        $preExisting = ElementAssignment::where('exam_id', $examId)->where('element_id', $this->attributes['id'])->first();
-//        if(! is_null($preExisting)) $preExisting->delete();
-
-        /*
-        Check to see if another element is assigned here
-        If it is, delete the existing assignment.
-        */
-//        $e = ElementAssignment::where('exam_id', $examId)->where('question_id', $questionId)->where('subtask', $subtask)->first();
-//        if(! is_null($e)) $e->delete();
-
-        //Create a new element assignment object and populate it with the new assignment
-        $newAssign = new ElementAssignment();
-        $newAssign->question_id = $questionId;
-        $newAssign->exam_id = $examId;
-        $newAssign->subtask = $subtask;
-        $newAssign->element_id = $this->attributes['id'];
-        //Save it
-        $newAssign->save();
-
-        return $this;
-    }
+//    /**
+//     * Records the element as a subtask of an assigned question.
+//     *
+//     *
+//     * @param $examId
+//     * @param $questionId
+//     * @param $subtask
+//     * @return $this
+//     */
+//    public function setAsQuestionTask($examId, $questionId, $subtask)
+//    {
+//        /*
+//        Check to see if the element is already assigned on the exam.
+//        If so, remove it so that there will only be one of an element per exam.
+//        */
+////        $preExisting = ElementAssignment::where('exam_id', $examId)->where('element_id', $this->attributes['id'])->first();
+////        if(! is_null($preExisting)) $preExisting->delete();
+//
+//        /*
+//        Check to see if another element is assigned here
+//        If it is, delete the existing assignment.
+//        */
+////        $e = ElementAssignment::where('exam_id', $examId)->where('question_id', $questionId)->where('subtask', $subtask)->first();
+////        if(! is_null($e)) $e->delete();
+//
+//        //Create a new element assignment object and populate it with the new assignment
+//        $newAssign = new ElementAssignment();
+//        $newAssign->question_id = $questionId;
+//        $newAssign->exam_id = $examId;
+//        $newAssign->subtask = $subtask;
+//        $newAssign->element_id = $this->attributes['id'];
+//        //Save it
+//        $newAssign->save();
+//
+//        return $this;
+//    }
 
 
     /**
--- tests/app/ElementTest.php
+++ tests/app/ElementTest.php
@@ -9,9 +9,9 @@
 namespace App;
 
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+//use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ElementTest extends \ReseedingTestCase
+class ElementTest extends \TestCase
 {
 //    use DatabaseMigrations;
     protected $object;
@@ -32,160 +32,165 @@ class ElementTest extends \ReseedingTestCase
     public function loginAndMakeElement()
     {
         \Auth::loginUsingId(self::$userid);
-        $this->element = Element::all()->random();
+        $this->element = factory(Element::class)->create();
         $this->object = new Element;
     }
 
-
-    /**
-     * @test
-     * @group slowTests
-     */
-    public function setAsQuestionTask_sets_without_disturbing_other_assignments()
-    {
-        /* Completely reset and re-seed the database */
-        $this->prepareDatabase();
-
-        /* At this point, the first question on exam 1 has question_id = 1.
-            There are five element assignments.
-            element_assignment_id = 1 =>
-                element_id = 1   subtask = 1  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
-            element_assignment_id = 2 =>
-                element_id = 2   subtask = 2  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1 )
-            ...
-            element_assignment_id = 5 =>
-                element_id = 5   subtask = 5  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
-
-            Also, the first element which is not on exam 1 is element_id = 26.
-        */
-        $target_exam_id = 1;
-        $element_id_to_add_to_exam = 26;
-
-        /*
-            So, we are going to take element_id = 26 and make it the new question 1 subtask 1. This should replace element_id =1
-            and not disturb any other element assignment.
-        */
-        $element_id_to_replace = 1;
-        $subtask_being_assigned_to = 1;
-        $question_id_being_assigned_to = 1;
-
-        //Prepare the victim.....
-        $this->object = Element::find($element_id_to_add_to_exam);
-
-        //call
-        $result = $this->object->setAsQuestionTask($target_exam_id, $question_id_being_assigned_to, $subtask_being_assigned_to);
-
-        //Check that gave back an element object
-        $this->assertInstanceOf('App\Element', $result);
-        //And check to make sure it is the expected one
-        $this->assertEquals($element_id_to_add_to_exam, $this->object->getId());
-
-        //Make sure that the element has been written into the database
-        $this->seeInDatabase('element_assignments',
-                             [
-                                 'question_id' => $question_id_being_assigned_to,
-                                 'exam_id' => $target_exam_id,
-                                 'element_id' => $this->object->getId(),
-                                 'subtask' => $subtask_being_assigned_to
-                             ]);
-        //Make sure that the target element has been overwritten (in case somehow the unique constraint got altered)
-        $this->notSeeInDatabase('element_assignments',
-                             [
-                                 'question_id' => $question_id_being_assigned_to,
-                                 'exam_id' => $target_exam_id,
-                                 'element_id' => $element_id_to_replace,
-                                 'subtask' => $subtask_being_assigned_to
-                             ]);
-
-        //Finally make sure that the other elements assigned to the question haven't been touched
-        for($i=2; $i<=5; $i++)
-        {
-            $this->seeInDatabase('element_assignments',
-                                 [
-                                     'question_id' => $question_id_being_assigned_to,
-                                     'exam_id' => $target_exam_id,
-                                     'element_id' => $i,
-                                     'subtask' => $i
-                                 ]);
-        }
-    }
-
-
-    /**
-     * @test
-     * @group slowTests
-     */
-    public function setAsQuestionTask_does_not_leave_duplicate_when_update_was_deletion()
-    {
-        /* Completely reset and re-seed the database */
-        $this->prepareDatabase();
-
-        /*
-         * In this case, we are supposing that subtask 1 was deleted.
-         * That means each of the other elements needs to move up one subtask.
-         * That is, we want to end up with
-                element_id = 2   subtask = 1  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
-                element_id = 3   subtask = 2  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
-                element_id = 4   subtask = 3  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
-                element_id = 5   subtask = 4  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
-        */
-        $target_exam_id = 1;
-        $question_id_being_assigned_to = 1;
-        /* Note that the tricky thing here is that we won't know coming in whether the update included just adding
-           new elements, reshuffling without deletion, or deletion; or some combination of those. So we are
-           going to assume that this function gets called successively for each element. As long as we clean up
-           any pre-existing assignments to that exam we should be okay.
-
-           For any but the very last subtask, the problem will be taken care of by overwriting in a subsequent call.
-           But that won't happen for the last subtask. So let's check that case to make sure everything is clean.
-           That is, we need to be particularly sure that when element_id = 5 gets recorded as subtask = 4, it isn't still assigned
-           as subtask = 5.
-         */
-        $element_id_to_reassign = 5;
-        $original_subtask_of_the_element = 5;
-        $subtask_being_assigned_to = 4;
-
-        //Prepare the victim.....
-        $this->object = Element::find($element_id_to_reassign);
-
-        //call
-        $result = $this->object->setAsQuestionTask($target_exam_id, $question_id_being_assigned_to, $subtask_being_assigned_to);
-
-        //Check that gave back an element object
-        $this->assertInstanceOf('App\Element', $result);
-        //And check to make sure it is the expected one
-        $this->assertEquals($element_id_to_reassign, $this->object->getId());
-
-        //Make sure that the element has been written into the new position
-        $this->seeInDatabase('element_assignments',
-                             [
-                                 'question_id' => $question_id_being_assigned_to,
-                                 'exam_id' => $target_exam_id,
-                                 'element_id' => $this->object->getId(),
-                                 'subtask' => $subtask_being_assigned_to
-                             ]);
-
-        //Make sure that its previous assignment was removed
-        $this->notSeeInDatabase('element_assignments',
-                                [
-                                    'question_id' => $question_id_being_assigned_to,
-                                    'exam_id' => $target_exam_id,
-                                    'element_id' => $element_id_to_reassign,
-                                    'subtask' => $original_subtask_of_the_element
-                                ]);
-
-        //Finally make sure that the other elements assigned to the question haven't been touched
-        for($i=1; $i<=3; $i++)
-        {
-            $this->seeInDatabase('element_assignments',
-                                 [
-                                     'question_id' => $question_id_being_assigned_to,
-                                     'exam_id' => $target_exam_id,
-                                     'element_id' => $i,
-                                     'subtask' => $i
-                                 ]);
-        }
-    }
+//
+//    /**
+//     * @test
+//     * @group slowTests
+//     */
+//    public function setAsQuestionTask_sets_without_disturbing_other_assignments()
+//    {
+//        /* Completely reset and re-seed the database */
+////        $this->prepareDatabase();
+//        $fixture = $this->makeElementAssignmentsForQuestion(5);
+//
+//        //Prepare the victim.....
+//        $this->object = factory(Element::class)->create();
+//
+//        /* At this point, the first question on exam 1 has question_id = 1.
+//            There are five element assignments.
+//            element_assignment_id = 1 =>
+//                element_id = 1   subtask = 1  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
+//            element_assignment_id = 2 =>
+//                element_id = 2   subtask = 2  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1 )
+//            ...
+//            element_assignment_id = 5 =>
+//                element_id = 5   subtask = 5  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
+//
+//            Also, the first element which is not on exam 1 is element_id = 26.
+//        */
+//        $target_exam_id = $fixture['exam']->id;
+//        $element_id_to_add_to_exam = $this->object->id;
+//
+//        /*
+//            So, we are going to take element_id = 26 and make it the new question 1 subtask 1. This should replace element_id =1
+//            and not disturb any other element assignment.
+//        */
+//        $element_id_to_replace = $fixture['elementIds'][0];
+//        $subtask_being_assigned_to = 1;
+//        $question_id_being_assigned_to = $fixture['question']->id;
+//
+//
+//        #call
+//        $result = $this->object->setAsQuestionTask($target_exam_id, $question_id_being_assigned_to, $subtask_being_assigned_to);
+//
+//        #check
+//        //Check that gave back an element object
+//        $this->assertInstanceOf('App\Element', $result);
+//        //And check to make sure it is the expected one
+//        $this->assertEquals($element_id_to_add_to_exam, $this->object->getId());
+//
+//        //Make sure that the element has been written into the database
+//        $this->seeInDatabase('element_assignments',
+//                             [
+//                                 'question_id' => $question_id_being_assigned_to,
+//                                 'exam_id' => $target_exam_id,
+//                                 'element_id' => $this->object->getId(),
+//                                 'subtask' => $subtask_being_assigned_to
+//                             ]);
+//        //Make sure that the target element has been overwritten (in case somehow the unique constraint got altered)
+//        $this->notSeeInDatabase('element_assignments',
+//                             [
+//                                 'question_id' => $question_id_being_assigned_to,
+//                                 'exam_id' => $target_exam_id,
+//                                 'element_id' => $element_id_to_replace,
+//                                 'subtask' => $subtask_being_assigned_to
+//                             ]);
+//
+//        //Finally make sure that the other elements assigned to the question haven't been touched
+//        for($i=2; $i<=5; $i++)
+//        {
+//            $this->seeInDatabase('element_assignments',
+//                                 [
+//                                     'question_id' => $question_id_being_assigned_to,
+//                                     'exam_id' => $target_exam_id,
+//                                     'element_id' => $i,
+//                                     'subtask' => $i
+//                                 ]);
+//        }
+//    }
+//
+//
+//    /**
+//     * @test
+//     * @group slowTests
+//     */
+//    public function setAsQuestionTask_does_not_leave_duplicate_when_update_was_deletion()
+//    {
+//        /* Completely reset and re-seed the database */
+//        //$this->prepareDatabase();
+//        $fixture = $this->makeElementAssignmentsForQuestion(5);
+//
+//        /*
+//         * In this case, we are supposing that subtask 1 was deleted.
+//         * That means each of the other elements needs to move up one subtask.
+//         * That is, we want to end up with
+//                element_id = 2   subtask = 1  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
+//                element_id = 3   subtask = 2  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
+//                element_id = 4   subtask = 3  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
+//                element_id = 5   subtask = 4  (exam_id = 1, question_number = 1, question_id = 1, question_assignment_id = 1)
+//        */
+//        $target_exam_id = $fixture['exam']->id;
+//        $question_id_being_assigned_to = $fixture['question']->id;
+//
+//        /* Note that the tricky thing here is that we won't know coming in whether the update included just adding
+//           new elements, reshuffling without deletion, or deletion; or some combination of those. So we are
+//           going to assume that this function gets called successively for each element. As long as we clean up
+//           any pre-existing assignments to that exam we should be okay.
+//
+//           For any but the very last subtask, the problem will be taken care of by overwriting in a subsequent call.
+//           But that won't happen for the last subtask. So let's check that case to make sure everything is clean.
+//           That is, we need to be particularly sure that when element_id = 5 gets recorded as subtask = 4, it isn't still assigned
+//           as subtask = 5.
+//         */
+//        $element_id_to_reassign = $fixture['elementIds'][4];
+//        $original_subtask_of_the_element = 5;
+//        $subtask_being_assigned_to = 4;
+//
+//        //Prepare the victim.....
+//        $this->object = Element::find($element_id_to_reassign);
+//
+//        //call
+//        $result = $this->object->setAsQuestionTask($target_exam_id, $question_id_being_assigned_to, $subtask_being_assigned_to);
+//
+//        //Check that gave back an element object
+//        $this->assertInstanceOf('App\Element', $result);
+//        //And check to make sure it is the expected one
+//        $this->assertEquals($element_id_to_reassign, $this->object->getId());
+//
+//        //Make sure that the element has been written into the new position
+//        $this->seeInDatabase('element_assignments',
+//                             [
+//                                 'question_id' => $question_id_being_assigned_to,
+//                                 'exam_id' => $target_exam_id,
+//                                 'element_id' => $this->object->getId(),
+//                                 'subtask' => $subtask_being_assigned_to
+//                             ]);
+//
+//        //Make sure that its previous assignment was removed
+//        $this->notSeeInDatabase('element_assignments',
+//                                [
+//                                    'question_id' => $question_id_being_assigned_to,
+//                                    'exam_id' => $target_exam_id,
+//                                    'element_id' => $element_id_to_reassign,
+//                                    'subtask' => $original_subtask_of_the_element
+//                                ]);
+//
+//        //Finally make sure that the other elements assigned to the question haven't been touched
+//        for($i=1; $i<=3; $i++)
+//        {
+//            $this->seeInDatabase('element_assignments',
+//                                 [
+//                                     'question_id' => $question_id_being_assigned_to,
+//                                     'exam_id' => $target_exam_id,
+//                                     'element_id' => $i,
+//                                     'subtask' => $i
+//                                 ]);
+//        }
+//    }
 
 
 
