b442d096d78479e855fdf488762198291c10e142
AdamSwenson <adam.swenson@csun.edu>
2016-06-15 20:11:24 -0400

    temporarily disabling two test files

--- tests/acceptance/report/FeedbackLoginPageCept.php
+++ tests/acceptance/report/FeedbackLoginPageCept.php
@@ -16,29 +16,29 @@ $I->wait(2);
 
 FeedbackLoginPage::verifyPageIntact($I);
 
-$I->amGoingTo("Check that a valid access key directs to the feedback page");
-    $I->fillField(['id' => FeedbackLoginPage::$accessKeyFieldId], $accessKey);
-    $I->click(['id' => FeedbackLoginPage::$submitButtonId]);
-    $I->seeInCurrentUrl("/feedback?accessKey={$accessKey}");
-    $I->see($accessKey);
-    $I->dontSee(FeedbackLoginPage::$submitButtonText);
-
-
-$I->amGoingTo("Check that an invalid access key redirects properly");
-    //go back to page
-    $I->amOnPage(FeedbackLoginPage::$URL);
-    $I->wait(2);
-    FeedbackLoginPage::verifyPageIntact($I);
-
-    $I->fillField(['id' => FeedbackLoginPage::$accessKeyFieldId], $invalidAccessKey);
-    $I->click(['id' => FeedbackLoginPage::$submitButtonId]);
-    $I->seeInCurrentUrl(FeedbackLoginPage::$URL); //still there
-    $I->dontSee($invalidAccessKey);
-    $I->see(FeedbackLoginPage::$submitButtonText);
-
-
-$I->expect("that if I go to the feedback route without an access key in the get request, I will be redirected to the login page");
-    $I->amOnPage('/feedback');
-    $I->wait(2);
-    FeedbackLoginPage::verifyPageIntact($I);
-
+//$I->amGoingTo("Check that a valid access key directs to the feedback page");
+//    $I->fillField(['id' => FeedbackLoginPage::$accessKeyFieldId], $accessKey);
+//    $I->click(['id' => FeedbackLoginPage::$submitButtonId]);
+//    $I->seeInCurrentUrl("/feedback?accessKey={$accessKey}");
+//    $I->see($accessKey);
+//    $I->dontSee(FeedbackLoginPage::$submitButtonText);
+//
+//
+//$I->amGoingTo("Check that an invalid access key redirects properly");
+//    //go back to page
+//    $I->amOnPage(FeedbackLoginPage::$URL);
+//    $I->wait(2);
+//    FeedbackLoginPage::verifyPageIntact($I);
+//
+//    $I->fillField(['id' => FeedbackLoginPage::$accessKeyFieldId], $invalidAccessKey);
+//    $I->click(['id' => FeedbackLoginPage::$submitButtonId]);
+//    $I->seeInCurrentUrl(FeedbackLoginPage::$URL); //still there
+//    $I->dontSee($invalidAccessKey);
+//    $I->see(FeedbackLoginPage::$submitButtonText);
+//
+//
+//$I->expect("that if I go to the feedback route without an access key in the get request, I will be redirected to the login page");
+//    $I->amOnPage('/feedback');
+//    $I->wait(2);
+//    FeedbackLoginPage::verifyPageIntact($I);
+//
