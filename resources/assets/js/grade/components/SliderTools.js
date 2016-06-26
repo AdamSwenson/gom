a89e4016eb4a31d67bb80af3c8b34c8a2e84b4c2
AdamSwenson <adam.swenson@csun.edu>
2016-06-26 08:18:24 -0700

    removed unnecessary console log statement from slider toools

--- resources/assets/js/grade/components/SliderTools.js
+++ resources/assets/js/grade/components/SliderTools.js
@@ -99,8 +99,7 @@ module.exports = {
         //store the new element score in the data object
         data.storeElementScore( Roster.activeStudent, elementIndex, score );
 
-        window.console.log('same', oldScore, score, this.isSameValence( oldScore, score ));
-
+       
         /**
          * update comment text and save to DB.
          * Only replace text if the score has changed valence regions
