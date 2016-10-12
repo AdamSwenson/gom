deb353feb45987116c693b25586a632a630d688f
AdamSwenson <adam.swenson@csun.edu>
2016-10-12 16:48:22 -0700

    added message about survey to all pages

--- public/js/report-student-controls-package.js
+++ public/js/report-student-controls-package.js
@@ -13343,7 +13343,7 @@ var delayTime = 5000;
  */
 module.exports = function () {
   //Automatically hide non-important flash message
-  $('div.alert').not('alert-important').delay(delayTime).slideUp(300);
+  $('div.alert').not('.alert-important').delay(delayTime).slideUp(300);
 };
 
 },{"jquery":15}],21:[function(require,module,exports){
