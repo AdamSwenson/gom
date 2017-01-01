f9cecb6e789075b43f75d97d498a655f2fea79e9
AdamSwenson <adam.swenson@csun.edu>
2016-12-31 17:41:51 -0800

    changed auth request messages

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -31,7 +31,10 @@ class AuthRequest extends Request
     }
     public function messages() {
         return [
+            'email' => "We can't find a user with that e-mail address",
             'email.required' => 'Your email is required',
+            'email.valid' => 'Please enter a valid email address'
+
         ];
 
     }
