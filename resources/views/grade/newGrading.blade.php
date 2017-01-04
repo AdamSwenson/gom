eee07de689f253388bb2a26d6fdf4661923a0e71
AdamSwenson <adam.swenson@csun.edu>
2017-01-04 14:52:32 -0800

    started trying to integrate vuex store into grading page. lots of work there.

--- resources/views/grade/newGrading.blade.php
+++ resources/views/grade/newGrading.blade.php
@@ -8,86 +8,89 @@ $grades = App\Repositories\Grade\GradeFactory::gradeJson();
 
 @section('otherCss')
     <link href="{{ asset('css/grade-package.css') }}" rel="stylesheet" type="text/css">
-    <script src="{{asset('js/grade-exam-data.js')}}"></script>
+    {{--<script src="{{asset('js/grade-exam-data.js')}}"></script>--}}
+
 @endsection
 
 @section('body')
-    <div id="gradeExamPage" class="row mainBodyLocator">
-        <!-- Left column holds questions and sliders -->
-        <div id="questionAndSliderColumn"
-             class="col-md-8 questionAndSliderColumn">
+    <div id="app">
+        <div id="gradeExamPage" class="row mainBodyLocator">
+            <!-- Left column holds questions and sliders -->
+            <div id="questionAndSliderColumn"
+                 class="col-md-8 questionAndSliderColumn">
 
-            <h3 data-exam-id="{{ $exam->getId() }}">
-                <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-                {{ $exam->getTerm() }}, {{ $exam->getYear() }} "{{ $exam->getName() }}"
-            </h3>
+                <h3 data-exam-id="{{ $exam->getId() }}">
+                    <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+                    {{ $exam->getTerm() }}, {{ $exam->getYear() }} "{{ $exam->getName() }}"
+                </h3>
 
-            <h4 id="selectPrompt">Select a student to begin grading</h4>
+                <h4 id="selectPrompt">Select a student to begin grading</h4>
 
-            <div id="questionArea"
-                 class="startHidden">
-                <!-- Create one Question Tab for each question -->
-                <ul class="nav nav-pills nav-justified">
-                    @foreach($questionAssignments as $qAssignment)
-                        <?php $qNumber = $qAssignment->getQuestionNumber(); ?>
-                        <li class='<?php echo $qNumber == 1 ? 'active' : ''; ?>'
-                            role="presentation">
-                            <a id="tabQuestion{{ $qNumber }}"
-                               href="#panelQuestion{{ $qNumber }}"
-                               title="Grade question {{ $qNumber }}"
-                               data-toggle="tab">
-                                Q{{ $qNumber }}</a>
-                        </li>
-                    @endforeach
-                </ul>
+                <div id="questionArea"
+                     class="startHidden">
+                    <!-- Create one Question Tab for each question -->
+                    <ul class="nav nav-pills nav-justified">
+                        @foreach($questionAssignments as $qAssignment)
+                            <?php $qNumber = $qAssignment->getQuestionNumber(); ?>
+                            <li class='<?php echo $qNumber == 1 ? 'active' : ''; ?>'
+                                role="presentation">
+                                <a id="tabQuestion{{ $qNumber }}"
+                                   href="#panelQuestion{{ $qNumber }}"
+                                   title="Grade question {{ $qNumber }}"
+                                   data-toggle="tab">
+                                    Q{{ $qNumber }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
 
-                <!-- question panel -->
-                @include('grade.new.partials.question_panel')
-            </div>
-        </div>
-
-        <!-- Right column holds Roster and Time info -->
-        <div id="rosterAndDashboardColumn"
-             class="col-md-4 rosterAndDashboardColumn">
-
-            <!-- student name and ID -->
-            <current-student-area></current-student-area>
-
-            <!-- graded / remaining counters -->
-            <dashboard-counts finished-link="{{ url('grade/') }}"></dashboard-counts>
-
-            <!-- student table shows the student roster -->
-            <div class="panel panel-default">
-                <student-table></student-table>
+                    <!-- question panel -->
+                    @include('grade.new.partials.question_panel')
+                </div>
             </div>
 
-            <!-- statistics area holds time info -->
-            <dashboard-timer></dashboard-timer>
+            <!-- Right column holds Roster and Time info -->
+            <div id="rosterAndDashboardColumn"
+                 class="col-md-4 rosterAndDashboardColumn">
 
+                <!-- student name and ID -->
+                <current-student-area></current-student-area>
+
+                <!-- graded / remaining counters -->
+                <dashboard-counts finished-link="{{ url('grade/') }}"></dashboard-counts>
+
+                <!-- student table shows the student roster -->
+                <div class="panel panel-default">
+                    <student-table></student-table>
+                </div>
+
+                <!-- statistics area holds time info -->
+                <dashboard-timer></dashboard-timer>
+
+            </div>
         </div>
     </div>
-
 @endsection
 
 @section('jsArea')
     {{--<script src="{{ asset('/js/dev/new-data-package.js')}}"></script>--}}
-
+    <script src="{{ asset('/js/dev/test-package.js') }}"></script>
     <script type="text/javascript">
-                <?php
-                $studentElementComments = json_encode($studentElementComments, JSON_FORCE_OBJECT);
-                $studentElementScores = json_encode($studentElementScores, JSON_FORCE_OBJECT);
-                $studentQuestionScores = json_encode($studentQuestionScores, JSON_FORCE_OBJECT);
-                $examGradingTimes = json_encode($examGradingTimes, JSON_FORCE_OBJECT);
-                $studentGrades = json_encode($studentGrades, JSON_FORCE_OBJECT);
-                $maxScores = json_encode($maxQuestionScores, JSON_FORCE_OBJECT);
-                $numQuestions = count($questionAssignments);
-                ?>
+            <?php
+            $studentElementComments = json_encode($studentElementComments, JSON_FORCE_OBJECT);
+            $studentElementScores = json_encode($studentElementScores, JSON_FORCE_OBJECT);
+            $studentQuestionScores = json_encode($studentQuestionScores, JSON_FORCE_OBJECT);
+            $examGradingTimes = json_encode($examGradingTimes, JSON_FORCE_OBJECT);
+            $studentGrades = json_encode($studentGrades, JSON_FORCE_OBJECT);
+            $maxScores = json_encode($maxQuestionScores, JSON_FORCE_OBJECT);
+            $numQuestions = count($questionAssignments);
+            ?>
 
         var activeTab = 'navGrade';
-//                GOM.store.activeStudent = 0;
-        var store = new Data();
-                store.activeStudent = 0;
-//                var store = GOM.store;
+        //                GOM.store.activeStudent = 0;
+           var store = document.body.Data.Store;
+//            var store = new Data();
+        {{--store.activeStudent = 0;--}}
+        {{--//                var store = GOM.store;--}}
 
         store.setExamId({!! $exam->id !!});
         store.loadStockComments({!! $stockCommentsJson !!});
