97c98b22a49019898a1a87798df5e0826df6c6aa
AdamSwenson <adam.swenson@csun.edu>
2017-05-26 20:23:12 -0700

    made almost all editable fields for exam, question, and element nullable in db

---
+++ database/migrations/2017_05_26_201905_make_element_display_text_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeElementDisplayTextNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('elements', function (Blueprint $table) {
+            $table->string('displayText')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
