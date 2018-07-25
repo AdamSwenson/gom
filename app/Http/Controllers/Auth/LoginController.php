2a01f840428d28ced61030352949ae961dd8ee3d
AdamSwenson <adam.swenson@csun.edu>
2018-07-25 14:35:12 -0700

    Post GOM-357 merge

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,6 +26,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = 'dev/setup';
+    
     //old version
     //    protected $redirectTo = '/exam';
 
