8c818f45e07111d141c8ca41499ba033c8155900
AdamSwenson <adam.swenson@csun.edu>
2018-08-07 17:14:21 -0700

    renamed css package for grading to hopefully avoid deleting it before GOM-372 is fixed

--- resources/views/new/newgrading.blade.php
+++ resources/views/new/newgrading.blade.php
@@ -1,10 +1,11 @@
 @extends('new.newmaster')
 
-@section('pageTitle', 'Grade exams')
+@section('pageTitle', 'Gradeomatic - Grade exams')
 @section('description', 'Grading page for the Gradeomatic')
 
 @section('pageCss')
-    <link href="{{ asset('css/grade-package.css') }}" rel="stylesheet" type="text/css">
+    {{--This needs to be here until GOM-372 is fixed--}}
+    <link href="{{ asset('css/grade-package-required-until-gom-372-is-fixed.css') }}" rel="stylesheet" type="text/css">
 @endsection
 
 @section('pageJs')
