22addc3a75ccb693780e652415c6423f4f6187a6
AdamSwenson <adam.swenson@csun.edu>
2019-04-12 17:18:17 -0700

    added additional buttons for releasing exam ala GOM-416

---
+++ resources/assets/js/development/components/setup/accesscontrols/send-notification-email-button.vue
@@ -0,0 +1,38 @@
+<script>
+    import buttonBase from '../../input/button-base';
+    import Payload from '../../../../models/Payload';
+    import * as aTypes from '../../../../store/action-types'
+
+    export default {
+        name: "send-notification-email-button",
+
+        extends: buttonBase,
+
+        data: function () {
+            return {
+                local: {
+                    buttonText: 'Email access codes to students',
+                    icon: "",
+                    identifyingClass: '',
+                    linkClass: '',
+                    linkTitle: '',
+                    screenReaderText: '',
+                }
+            }
+        },
+
+        methods: {
+            handleClick: function () {
+                let pl = Payload.factory( { obj: this.exam } );
+                window.console.log( this.name, pl );
+
+            }
+        }
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
