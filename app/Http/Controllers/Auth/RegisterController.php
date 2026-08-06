d066ede166500f5dd50dd00fb9a4140695967821
Adam Swenson <adamswenson@gmail.com>
2026-08-06 14:48:27 -0700

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,88 +3,45 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Events\NewUserSignedUpEvent;
-use App\User;
-use Validator;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use App\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\View\View;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
+    protected string $redirectTo = 'dev/setup';
 
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = 'dev/setup';
-
-    //old version
-//    protected $redirectTo = '/exam/create';
-
-    /**
-     * Create a new controller instance.
-     *
-     */
     public function __construct()
     {
-
-        $this->middleware('guest');
-        $this->middleware('restrictRegistration');
+        $this->middleware(['guest', 'restrictRegistration']);
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
+    public function showRegistrationForm(): View
     {
-
-        $rules = [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-        ];
-
-        $messages = [
-            'email.required' => 'Your email is required',
-            'email.email' => 'Please enter a valid email address',
-        ];
-
-        return Validator::make($data, $rules, $messages);
-
+        return view('auth.register');
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
+    public function register(Request $request): RedirectResponse
     {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
         $user = User::create([
-                                 'name' => $data['name'],
-                                 'email' => $data['email'],
-                                 'password' => bcrypt($data['password']),
-                             ]);
-        //Trigger new registration event
-        event( new NewUserSignedUpEvent($user) );
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
 
-        //return the user so RegistersUsers Trait can continue logging in
-        return $user;
+        event(new NewUserSignedUpEvent($user));
+        Auth::login($user);
+
+        return redirect($this->redirectTo);
     }
 }
